475c8aca81938cf908e193acf8088b45503489ac
Diego Pagini <diegopaginidev@gmail.com>
2023-01-05 20:04:00 +0100

    config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { join } from 'path';
+import { ItemsModule } from './items/items.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      driver: ApolloDriver,
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault],
+    }),
+    ItemsModule,
+  ],
   controllers: [],
   providers: [],
 })
