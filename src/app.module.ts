750d7356a569768ccdf53fc6c9fd895c638afc43
Diego Pagini <diegopaginidev@gmail.com>
2023-01-06 21:36:18 +0100

    postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,31 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 import { join } from 'path';
+
 import { ItemsModule } from './items/items.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       driver: ApolloDriver,
       playground: false,
       plugins: [ApolloServerPluginLandingPageLocalDefault],
+    }),
+    TypeOrmModule.forRoot({
+      autoLoadEntities: true,
+      database: process.env.DB_NAME,
+      host: process.env.DB_HOST,
+      password: process.env.DB_PASSWORD,
+      port: +process.env.DB_PORT,
+      synchronize: true,
+      type: 'postgres',
+      username: process.env.DB_USERNAME,
     }),
     ItemsModule,
   ],
