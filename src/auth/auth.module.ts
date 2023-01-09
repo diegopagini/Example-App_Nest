8eaeb0881682400ec391bd04a048dc4ce8126968
Diego Pagini <diego.pagini@neoris.com>
2023-01-09 17:13:54 +0100

    passport

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 import { UsersModule } from '../users/users.module';
 import { AuthResolver } from './auth.resolver';
@@ -6,6 +9,22 @@ import { AuthService } from './auth.service';
 
 @Module({
   providers: [AuthResolver, AuthService],
-  imports: [UsersModule],
+  imports: [
+    ConfigModule,
+    // Passport
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    // JWT
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: {
+          expiresIn: '4h',
+        },
+      }),
+    }),
+    UsersModule,
+  ],
 })
 export class AuthModule {}
