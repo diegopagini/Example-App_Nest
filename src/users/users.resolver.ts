0bc6aa53125c04b188607b102501b9f34eea378d
Diego Pagini <diego.pagini@neoris.com>
2023-01-10 11:56:24 +0100

    validate requests

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -1,10 +1,15 @@
+import { UseGuards } from '@nestjs/common';
 import { Args, ID, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
+import { ValidRoles } from 'src/auth/enums/valid-roles.enum';
 
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { ValidRolesArgs } from './dto/args/roles.arg';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 
 @Resolver(() => User)
+@UseGuards(JwtAuthGuard)
 export class UsersResolver {
   constructor(private readonly usersService: UsersService) {}
 
@@ -13,7 +18,10 @@ export class UsersResolver {
    * @returns Promise<User[]>
    */
   @Query(() => [User], { name: 'users' })
-  findAll(@Args() validRoles: ValidRolesArgs): Promise<User[]> {
+  findAll(
+    @Args() validRoles: ValidRolesArgs,
+    @CurrentUser([ValidRoles.admin]) user: User,
+  ): Promise<User[]> {
     return this.usersService.findAll(validRoles.roles);
   }
 
@@ -23,7 +31,10 @@ export class UsersResolver {
    * @returns Promise<User>
    */
   @Query(() => User, { name: 'user' })
-  findOne(@Args('id', { type: () => ID }) id: string): Promise<User> {
+  findOne(
+    @Args('id', { type: () => ID }) id: string,
+    @CurrentUser([ValidRoles.admin]) user: User,
+  ): Promise<User> {
     return;
   }
 
@@ -33,7 +44,10 @@ export class UsersResolver {
    * @returns Promise<User>
    */
   @Mutation(() => User)
-  blockUser(@Args('id', { type: () => ID }) id: string): Promise<User> {
+  blockUser(
+    @Args('id', { type: () => ID }) id: string,
+    @CurrentUser([ValidRoles.admin]) user: User,
+  ): Promise<User> {
     return this.usersService.block(id);
   }
 }
